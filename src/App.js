079a3a6d8a6d9a8d2e3d1d53004fbf1204de70e0
shelooks16 <blauyedz@zoho.eu>
2020-05-15 00:23:28 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Parcel Micro App!</h1>
+      <p>Hard to get more minimal.</p>
+    </>
+  );
+};
+
+export default App;
