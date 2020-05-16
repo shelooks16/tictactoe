df1a56fe41819a4e6bbfc31e4761d69dae229396
shelooks16 <blauyedz@zoho.eu>
2020-05-16 17:03:48 +0300

    calculate winner and lift up the state

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,28 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-
-  const handleSquareClick = position => {
-    if (board[position]) {
-      return;
-    }
-
-    setBoard(prev => {
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isXNext ? 'X' : 'O';
-        }
-
-        return square;
-      });
-    });
-
-    setIsXNext(prev => !prev);
-  };
-
+const Board = ({ board, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
